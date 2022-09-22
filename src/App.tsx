24b973e68742bcb0e8fd2e27eb7aa7ef352d2fdc
vrckomark <vrckomark@gmail.com>
2022-09-22 15:56:22 +0200

    big bang

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    await axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-5xl">
+        <h1>TO JE H1</h1>
+        <h2>TO JE H2</h2>
+      </div>
     </div>
   );
 }
---
+++ src/components/CoinItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CoinItem({ data }: any) {
+  return (
+    <>
+      <div>{data}</div>
+    </>
+  );
+}
